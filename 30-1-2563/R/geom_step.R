0b614a34ae0a3309b7cf439756222964e27b0f8e
BluebirdLyrase <38246181+BluebirdLyrase@users.noreply.github.com>
2020-01-30 10:01:32 +0700

    line colororrr

--- 30-1-2563/R/geom_step.R
+++ 30-1-2563/R/geom_step.R
@@ -1,14 +1,13 @@
 library("tidyverse")
 
-setwd("")
-
 rm(list=ls())
 
-pop <- read.csv("data/us-postage.csv",sep=",",header=TRUE)
+postage <- read.csv("data/us-postage.csv",sep=",",header=TRUE)
 
-ggplot(pop,aes(x=pop$Year, y=pop$Price)) + 
+ggplot(postage,aes(x=postage$Year, y=postage$Price)) + 
   geom_step(color='red',size=1) +
   xlab("Year")+ylab("Population (Billion people)") +
   scale_y_continuous(breaks = c(seq(0.25,0.50,0.05)), 
-                     labels = c(seq(0.25,0.50,0.05)),
                      limits = c(0.25,0.50))
+  # +scale_x_continuous(breaks = c(seq(min(postage$Year),max(postage$Year)+1,2)), 
+   #                  limits = c(min(min(postage$Year),max(postage$Year)+1)))
--- 30-1-2563/R/w06_loess_pre.R
+++ 30-1-2563/R/w06_loess_pre.R
@@ -5,8 +5,12 @@ unemployment[1:10,]
 # Plain scatter plot
 plot(1:length(unemployment$Value), unemployment$Value, cex=.3)
 
+
 # Fitted linear line
 lines(c(1,746), c(4.889523,6.42593))
 
 # Scatter plot with loess
-scatter.smooth(x=1:length(unemployment$Value), y=unemployment$Value, cex=.3,degree=2,span=0.9)
+scatter.smooth(x=1:length(unemployment$Value), y=unemployment$Value, cex=.3,degree=2,span=0.5,col="blue", 
+               lpars = list(col = "red"))
+#lower the span more smooth the scatter be
+#lpars = list(col = "red", lwd = 3, lty = 3)
